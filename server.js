ded7506197c4d3e9c0faea734334600783072b52
Evan Hardek <evanhardek@gmail.com>
2021-01-12 21:35:56 -0500

    added port

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const fs = require("fs")
-const db = require("./db/db.json")
-
-const PORT = process.env.PORT || 8000;
+const db = require("./db/db.json");
+const { dirname } = require("path");
 const app = express ()
-app.use(express.static(__dirname + '/public'));
+
+const PORT = process.env.PORT || 8080;
+
+
+app.use(express.static(dirname + '/public'));
 app.use(express.static('./'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 require("./apiRoutes")(app);
-require("./htmlRoutes")
+require("./htmlRoutes")(app);
 
-
+app.listen(PORT,function(){
+    console.log("App listening on PORT " + PORT);
+});
 
